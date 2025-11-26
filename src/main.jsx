bdb2e4c92fc587d960ebb3d0d2c33374d94d57cf
amygdala2025 <amygdalaconrad@gmail.com>
2025-11-26 23:04:04 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-import "./index.css"; // 선택
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    {/* basename은 기본값(/)로 두면 됨. custom domain에서도 / 를 그대로 쓴다 */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
